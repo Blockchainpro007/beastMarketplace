fac810a1502268214e380351fd64709002315dd6
DavidBlockDev <115107169+DavidBlockDev@users.noreply.github.com>
2022-10-06 08:08:13 +0900

    mod for heroku

--- index.js
+++ index.js
@@ -1,30 +1,59 @@
-const express = require('express');
-const cors = require('cors');
-const connectDB = require('./config/db');
-const path = require('path');
 
-const app = express();
+import express from "express"
+import mongoose from "mongoose"
+import 'dotenv/config'
+import cors from 'cors'
 
-app.use(cors());
+import authRoutes from "./routes/users.js"
 
-// Connect Database
-connectDB();
+import decryptoin from "./middleware/decryption.js"
+import morgan from "morgan"
+import path from "path"
+import fs from "fs"
+import bodyParser from "body-parser"
+import cookieParser from 'cookie-parser'
 
-// Init Middleware
-app.use(express.json());
 
-// Define Routes
-app.use('/api/users', require('./routes/users'));
+const app = express()
+app.use(express.json())
+
+var accessLogStream = fs.createWriteStream(path.join('logos/', 'access.log'), { flags: 'a' })
+// setup the logger
+app.use(morgan('combined', { stream: accessLogStream }))
+
+// install middleware for decryption
+app.use(decryptoin)
+
+// Routes
+// ------------------------------------
+app.use(cors())
+app.use(bodyParser.json())
+app.use(cookieParser());
+
+app.use("/auth", authRoutes)
+
+// Connection With Database
+mongoose.connect(process.env.MONGO_URI).then((res)=>{
+    console.log("connected to DB")
+})
+.catch((err)=> console.log(err.message))
+
+app.get('/', (req, res) => {
+    res.send("We are on home today")
+})
 
 // Serve static assets in production
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static('client/build'));
+    app.use(express.static('client/build'));
+  
+    app.get('*', (req, res) => {
+      res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    });
+  }
+  
+  const PORT = process.env.PORT || 5000;
+  
+  app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
 
-  app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-  });
-}
-
-const PORT = process.env.PORT || 5000;
-
-app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
+// app listening 
+// app.listen(8000)
